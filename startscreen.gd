68de7f539b5c02b0073f20f153193bb3ddca2777
Joshua Schmidt <jcschmidt31@outlook.com>
2025-01-25 08:11:44 -0600

    Added StartScreen

---
+++ startscreen.gd
@@ -0,0 +1,13 @@
+extends Control
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	var button = $Button
+	if button:
+		button.connect("pressed", _on_StartButton_pressed)
+	else:
+		print("Button node not found")
+
+func _on_StartButton_pressed():
+	# Replace "res://game_singleplayer.tscn" with the actual path to your main scene
+	get_tree().change_scene_to_file("res://game_singleplayer.tscn")
