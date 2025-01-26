973c12d837acd05f3b75d62ca1ee5b84fb2cd5e5
Joshua Schmidt <jcschmidt31@outlook.com>
2025-01-25 18:45:22 -0600

    Level 1 Buildout

--- startscreen.gd
+++ startscreen.gd
@@ -1,13 +1,13 @@
 extends Control
 
 # Called when the node enters the scene tree for the first time.
-func _ready():
-	var button = $Button
+func _ready() -> void:
+	var button: Button = $Button
 	if button:
 		button.connect("pressed", _on_StartButton_pressed)
 	else:
 		print("Button node not found")
 
-func _on_StartButton_pressed():
+func _on_StartButton_pressed() -> void:
 	# Replace "res://game_singleplayer.tscn" with the actual path to your main scene
 	get_tree().change_scene_to_file("res://game_singleplayer.tscn")
